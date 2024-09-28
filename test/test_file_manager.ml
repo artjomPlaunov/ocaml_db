05b6834318edb014a649d80d990dba0c9ce125c9
Artjom Plaunov <artyemnyc@gmail.com>
2024-09-28 00:12:02 -0400

    add some alcotests

--- test/test_file_manager.ml
+++ test/test_file_manager.ml
@@ -14,15 +14,32 @@ module To_test = struct
     let s1 = Printf.sprintf "offset %d contains %d," pos2 (Int32.to_int (File.Page.get_int32 p2 pos2)) in 
     let s2 = Printf.sprintf "offset %d constains %s" pos1 (File.Page.get_string p2 pos1) in 
     s1 ^ s2
-  end 
+
+  let test2 = 
+    let f_mgr = File.FileManager.make "db" 4096 in 
+    let blocksize = File.FileManager.get_blocksize f_mgr in
+    let block = File.BlockId.make "tbl1" 2 in 
+    let page1 = File.Page.make blocksize in 
+    let pos1 = 4091 in
+    let _ = File.Page.set_string page1 pos1 "a" in 
+    let _ = File.FileManager.write f_mgr block page1 in 
+    let page2 = File.Page.make blocksize in 
+    let _ = File.FileManager.read f_mgr block page2 in 
+    Printf.sprintf "offset %d contains %s" pos1 (File.Page.get_string page2 pos1)
+  end
+
 
 let test1 () = 
   Alcotest.(check string) "same string" "offset 105 contains 69,offset 88 constains abcdefghijklm" (To_test.test1) 
+  
+let test2 () = 
+  Alcotest.(check string) "same string" "offset 4091 contains a" (To_test.test2)
 
 let () = 
   let open Alcotest in 
   run "FileManager" [
     "simple tests", [
-      test_case "Test 1" `Quick test1
-    ]
+      test_case "Test 1" `Quick test1;
+      test_case "Test 2" `Quick test2;
+    ];
   ]
