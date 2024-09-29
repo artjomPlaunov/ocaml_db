3f86525b38109dc8c05f244d7249147944b77ce3
tsuy <tchow@tutamail.com>
2024-09-29 01:44:44 -0400

    correct typo + move `make` to disambiguate

--- file/block_id.ml
+++ file/block_id.ml
@@ -1,5 +1,5 @@
 type t = string * int
 
+let make ~filename ~block_num = (filename, block_num)
 let file_name (filename, _) = filename
 let block_num (_, block_num) = block_num
-let make ~filename ~block_num = (file_name, block_num)
