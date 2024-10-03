a92c836b4d9f51f492fb6c67ce4828c2a828e42b
tsuy <tchow@tutamail.com>
2024-10-02 22:36:51 -0400

    rename file manager tests

--- test/file_manager_tests.ml
+++ test/file_manager_tests.ml
@@ -1,10 +1,13 @@
 module To_test = struct
   open File
 
-  let test1 =
-    let file_mgr = File_manager.make ~db_dirname:"db_test1" ~block_size:400 in
+  let write_and_read_string_and_int32 () =
+    let file_mgr =
+      File_manager.make ~db_dirname:"db_test_write_read_string_int32"
+        ~block_size:400
+    in
     let block_size = File_manager.get_blocksize file_mgr in
-    (* write a string "abcdefghijklm" and number 69 into block starting at position 88 *)
+    (* write a string "abcdefghijklm" and int 69 into block starting at position 88 *)
     let block = Block_id.make ~filename:"file1" ~block_num:100 in
     let page1 = Page.make ~block_size in
     let str_pos = 88 in
@@ -13,24 +16,27 @@ module To_test = struct
     let num_pos = str_pos + size in
     Page.set_int32 page1 num_pos (Int32.of_int 69);
     File_manager.write file_mgr block page1;
-    (* read block the block back into new page (page2) to make sure contents stay the same *)
+    (* read the block back into a new page (page2) to ensure contents stay the same *)
     let page2 = Page.make ~block_size in
     File_manager.read file_mgr block page2;
     assert (page1 = page2);
-    let s1 =
-      Printf.sprintf "offset %d constains %s, " str_pos
+    let str_offset_msg =
+      Printf.sprintf "offset %d contains %s, " str_pos
         (Page.get_string page2 str_pos)
     in
-    let s2 =
+    let int32_offset_msg =
       Printf.sprintf "offset %d contains %d" num_pos
         (Int32.to_int (Page.get_int32 page2 num_pos))
     in
-    s1 ^ s2
+    str_offset_msg ^ int32_offset_msg
 
-  let test2 =
-    let file_mgr = File_manager.make ~db_dirname:"db_test2" ~block_size:4096 in
+  let write_and_read_last_byte () =
+    let file_mgr =
+      File_manager.make ~db_dirname:"db_test_write_read_last_byte"
+        ~block_size:4096
+    in
     let block_size = File_manager.get_blocksize file_mgr in
-    (* write to the last byte in a block *)
+    (* Write to the last byte in a block *)
     let block = Block_id.make ~filename:"file2" ~block_num:2 in
     let page1 = Page.make ~block_size in
     let pos1 = 4091 in
@@ -41,16 +47,21 @@ module To_test = struct
     Printf.sprintf "offset %d contains %s" pos1 (Page.get_string page2 pos1)
 end
 
-let test1 () =
+let test_write_and_read_string_and_int32 () =
   Alcotest.(check string)
-    "same string" "offset 88 constains abcdefghijklm, offset 105 contains 69"
-    To_test.test1
+    "verify string and int32 are correctly written and read back"
+    "offset 88 contains abcdefghijklm, offset 105 contains 69"
+    (To_test.write_and_read_string_and_int32 ())
 
-let test2 () =
-  Alcotest.(check string) "same string" "offset 4091 contains a" To_test.test2
+let test_write_and_read_last_byte () =
+  Alcotest.(check string)
+    "verify last byte write and read" "offset 4091 contains a"
+    (To_test.write_and_read_last_byte ())
 
 let all_tests () =
   [
-    Alcotest.test_case "Test 1" `Quick test1;
-    Alcotest.test_case "Test 2" `Quick test2;
+    Alcotest.test_case "write and read string and int32" `Quick
+      test_write_and_read_string_and_int32;
+    Alcotest.test_case "write and read last byte" `Quick
+      test_write_and_read_last_byte;
   ]
