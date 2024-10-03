d9d72abdea7722b99450d36cbcecacaafe32eb04
Artjom Plaunov <artyemnyc@gmail.com>
2024-10-03 00:01:47 -0400

    buffer_manager test4

--- test/buffer_manager_tests.ml
+++ test/buffer_manager_tests.ml
@@ -93,17 +93,61 @@ module To_test = struct
     (* Now we should be able to pin block3, after unpinning block2. *)
     let buf5 = Buffer_manager.pin buffer_manager block3 in
     s1 ^ s2
+
+  (* Check that pinning twice on same block requires unpinning two times before pin. *)
+  let test4 =
+    let file_manager =
+      File.File_manager.make ~db_dirname:"buffertest4" ~block_size:1024
+    in
+    let log_file = "buffer_manager_test_log" in
+    let log_manager = Log_manager.make ~file_manager ~log_file in
+    let buffer_manager =
+      Buffer_manager.make ~file_manager ~log_manager ~num_buffers:1 ()
+    in
+    let block0 = Block_id.make ~filename:"testfile" ~block_num:0 in
+    let block1 = Block_id.make ~filename:"testfile" ~block_num:1 in
+    let buf0 = Buffer_manager.pin buffer_manager block0 in
+    let buf0 = Buffer_manager.pin buffer_manager block0 in
+    Buffer_manager.unpin buffer_manager buf0;
+    let s1 = Printf.sprintf "Attempting to pin block 1..." in
+    let s2 =
+      try
+        let _ = Buffer_manager.pin buffer_manager block1 in
+        "test fail"
+      with Buffer_manager.BufferAbortException ->
+        Printf.sprintf "Exception: No available Buffers..."
+    in
+    Buffer_manager.unpin buffer_manager buf0;
+    let buf1 = Buffer_manager.pin buffer_manager block1 in
+    let s3 =
+      try
+        let _ = Buffer_manager.pin buffer_manager block1 in
+        "Successfully pinned..."
+      with Buffer_manager.BufferAbortException -> Printf.sprintf "test fail"
+    in
+    s1 ^ s2 ^ s3
 end
-
-
 
 let test1 () = Alcotest.(check bool) "bool equality" true To_test.test1
 let test2 () = Alcotest.(check bool) "bool equality" true To_test.test2
-let test3 () = Alcotest.(check string) "same string" "Attempting to pin block 3...\nException: No available Buffers" To_test.test3
+
+let test3 () =
+  Alcotest.(check string)
+    "same string"
+    "Attempting to pin block 3...\nException: No available Buffers"
+    To_test.test3
+
+let test4 () =
+  Alcotest.(check string)
+    "same string"
+    "Attempting to pin block 1...Exception: No available \
+     Buffers...Successfully pinned..."
+    To_test.test4
 
 let all_tests () =
   [
     Alcotest.test_case "Test 1" `Quick test1;
     Alcotest.test_case "Test 2" `Quick test2;
     Alcotest.test_case "Test 3" `Quick test3;
+    Alcotest.test_case "Test 4" `Quick test4;
   ]
