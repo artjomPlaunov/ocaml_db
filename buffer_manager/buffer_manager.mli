255fb9c81cea95d6b2c5bafbae5621ed23c7adae
tsuy <tchow@tutamail.com>
2024-10-02 23:42:14 -0400

    add interface for buffer manager

---
+++ buffer_manager/buffer_manager.mli
@@ -0,0 +1,18 @@
+open File
+
+type t
+
+exception BufferAbortException
+
+val make :
+  ?max_wait_time:int ->
+  file_manager:File_manager.t ->
+  log_manager:Log_manager.t ->
+  num_buffers:int ->
+  unit ->
+  t
+
+val available : t -> int
+val flush_all : t -> int -> unit
+val unpin : t -> Db_buffer.t -> unit
+val pin : t -> Block_id.t -> Db_buffer.t
