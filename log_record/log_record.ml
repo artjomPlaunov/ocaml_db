3547cacf73a792f26c0e4a7dbccb2acd8264bee7
tsuy <tchow@tutamail.com>
2024-10-05 16:15:50 -0400

    makes for commit start and rollback

--- log_record/log_record.ml
+++ log_record/log_record.ml
@@ -2,8 +2,16 @@ open File
 
 type t =
   | Checkpoint
-  | Start
-  | Commit
+  | Start of {
+      mutable tx_num : int;
+      mutable offset : int;
+      mutable block : Block_id.t;
+    }
+  | Commit of {
+      mutable tx_num : int;
+      mutable offset : int;
+      mutable block : Block_id.t;
+    }
   | UpdateInt of {
       mutable tx_num : int;
       mutable offset : int;
@@ -16,7 +24,11 @@ type t =
       mutable value : string;
       mutable block : Block_id.t;
     }
-  | Rollback
+  | Rollback of {
+      mutable tx_num : int;
+      mutable offset : int;
+      mutable block : Block_id.t;
+    }
 
 let make_update_int_record page =
   let tx_pos = 4 in
@@ -46,13 +58,49 @@ let make_update_string_record page =
   let block = Block_id.make ~filename ~block_num in
   UpdateString { tx_num; offset; value; block }
 
+let make_start_record page =
+  let tx_pos = 4 in
+  let filename_pos = tx_pos + 4 in
+  let filename = Page.get_string page filename_pos in
+  let block_pos = filename_pos + Page.max_len (String.length filename) in
+  let block_num = Page.get_int32 page block_pos |> Int32.to_int in
+  let offset_pos = block_pos + 4 in
+  let tx_num = Page.get_int32 page tx_pos |> Int32.to_int in
+  let offset = Page.get_int32 page offset_pos |> Int32.to_int in
+  let block = Block_id.make ~filename ~block_num in
+  Start { tx_num; offset; block }
+
+let make_commit_record page =
+  let tx_pos = 4 in
+  let filename_pos = tx_pos + 4 in
+  let filename = Page.get_string page filename_pos in
+  let block_pos = filename_pos + Page.max_len (String.length filename) in
+  let block_num = Page.get_int32 page block_pos |> Int32.to_int in
+  let offset_pos = block_pos + 4 in
+  let tx_num = Page.get_int32 page tx_pos |> Int32.to_int in
+  let offset = Page.get_int32 page offset_pos |> Int32.to_int in
+  let block = Block_id.make ~filename ~block_num in
+  Commit { tx_num; offset; block }
+
+let make_rollback_record page =
+  let tx_pos = 4 in
+  let filename_pos = tx_pos + 4 in
+  let filename = Page.get_string page filename_pos in
+  let block_pos = filename_pos + Page.max_len (String.length filename) in
+  let block_num = Page.get_int32 page block_pos |> Int32.to_int in
+  let offset_pos = block_pos + 4 in
+  let tx_num = Page.get_int32 page tx_pos |> Int32.to_int in
+  let offset = Page.get_int32 page offset_pos |> Int32.to_int in
+  let block = Block_id.make ~filename ~block_num in
+  Rollback { tx_num; offset; block }
+
 let make ~byte =
   let page = Page.from_bytes byte in
   match Page.get_int32 page 0 |> Int32.to_int with
   | 0 -> failwith "checkpoint record"
-  | 1 -> failwith "start record"
-  | 2 -> failwith "commit record"
+  | 1 -> make_start_record page
+  | 2 -> make_commit_record page
   | 3 -> make_update_int_record page
   | 4 -> make_update_string_record page
-  | 5 -> failwith "rollback record"
+  | 5 -> make_rollback_record page
   | _ -> failwith "we're dead"
