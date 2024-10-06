f052a137ee7e58f08c39d7ffa3f476e1ce437ee2
Artjom Plaunov <artyemnyc@gmail.com>
2024-10-06 18:01:36 -0400

    log record test

--- test/log_record_tests.ml
+++ test/log_record_tests.ml
@@ -1,20 +1,19 @@
 module To_test = struct
   open File
 
-  let create_int_log =
-    Printf.printf "fdasfas";
+  let create_int_log () =
     let file_manager =
       File_manager.make ~db_dirname:"db_test_create_int_logs" ~block_size:1024 in
     let log_file_name = "int_logs" in
     let log_manager = Log_manager.make ~file_manager ~log_file:log_file_name in
     let blk = Block_id.make ~filename:"fname" ~block_num:1 in 
     let n = Log_record.write_update_int_log_record log_manager 15 blk 255 (Int32.of_int 15) in 
-    "aa"
-
+    let _ = Log_manager.flush log_manager 1 in 
+    Printf.sprintf "%d\n" n
 end
 
 let test_create_log () =
   Alcotest.(check string)
-    "same string" "lol" To_test.create_int_log
+    "same string" "lol" (To_test.create_int_log ())
 
 let all_tests () = [ Alcotest.test_case "create logs" `Quick test_create_log ]
