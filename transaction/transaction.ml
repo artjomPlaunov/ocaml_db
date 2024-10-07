627f5ee6ab6b91fbd82ba8635dc0f740283b830c
Artjom Plaunov <artyemnyc@gmail.com>
2024-10-07 12:59:10 -0400

    start implementing transactions

--- transaction/transaction.ml
+++ transaction/transaction.ml
@@ -7,6 +7,7 @@ type t = {
   file_manager : File_manager.t;
   log_manager : Log_manager.t;
   tx_num : int;
+  buffers: Transaction__Buffer_list.t;
 }
 
 let make ~file_manager ~log_manager ~buffer_manager =
@@ -17,8 +18,18 @@ let make ~file_manager ~log_manager ~buffer_manager =
     file_manager;
     log_manager;
     tx_num = -1;
+    buffers =  Buffer_list.make ~buffer_mgr:buffer_manager;
   }
 
-let commit transaction = failwith "todo"
+let commit tx =
+  (* recoveryMgr.commit code *)
+  Buffer_manager.flush_all tx.buffer_manager tx.tx_num;
+  let lsn = Log_record.write_commit_log_record tx.log_manager tx.tx_num in
+  Log_manager.flush tx.log_manager lsn;
+  Transaction__Buffer_list 
+  Printf.printf "transaction %d committed" tx.tx_num
+  (* concurMgr.release() *)
+
+  
 let rollback transaction = failwith "todo"
 let recover transaction = failwith "todo"
