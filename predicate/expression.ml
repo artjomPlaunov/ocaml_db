46411f8d9dc21343da5eb16e8980cd4418fd9a3a
tsuy <tchow@tutamail.com>
2024-10-29 13:24:13 -0400

    fix warnings with labeled args

--- predicate/expression.ml
+++ predicate/expression.ml
@@ -7,17 +7,21 @@ exception NotFieldNameExpr
 
 let make_const c = Const c
 let make_field_name f = FieldName f
-let is_field_name t = match t with Const _ -> false | _ -> true
-let get_constant e = match e with Const c -> c | _ -> raise NotConstExpr
+let is_field_name term = match term with Const _ -> false | _ -> true
+let get_constant expr = match expr with Const c -> c | _ -> raise NotConstExpr
 
-let get_field_name e =
-  match e with Const _ -> raise NotFieldNameExpr | FieldName f -> f
+let get_field_name expr =
+  match expr with Const _ -> raise NotFieldNameExpr | FieldName f -> f
 
-let eval e s =
-  match e with Const _ -> raise NotConstExpr | FieldName f -> Scan.get_val s f
+let eval expr scan =
+  match expr with
+  | Const _ -> raise NotConstExpr
+  | FieldName field_name -> Scan.get_val ~scan ~field_name
 
-let applies_to_schema e s =
-  match e with Const _ -> true | FieldName f -> Schema.has_field s f
+let applies_to_schema expr scan =
+  match expr with
+  | Const _ -> true
+  | FieldName field_name -> Schema.has_field scan field_name
 
-let to_string e =
-  match e with Const c -> Constant.to_string c | FieldName f -> f
+let to_string expr =
+  match expr with Const c -> Constant.to_string c | FieldName f -> f
--- scan/scan.mli
+++ scan/scan.mli
@@ -1,10 +1,17 @@
 type t
 
 (* Scan methods *)
-val before_first : t -> unit
-val next : t -> bool
-val get_int32 : t -> string -> int32
-val get_string : t -> string -> string
-val get_val : t -> string -> Constant.t
-val has_field : t -> string -> bool
-val close : t -> unit
+val get_rid : scan:t -> Record_id.t
+val move_to_rid : scan:t -> rid:Record_id.t -> unit
+val delete : scan:t -> unit
+val insert : scan:t -> unit
+val set_string : scan:t -> field_name:string -> value:string -> unit
+val set_int32 : scan:t -> field_name:string -> value:Int32.t -> unit
+val set_val : scan:t -> field_name:string -> value:Constant.t -> unit
+val before_first : scan:t -> unit
+val next : scan:t -> bool
+val get_int32 : scan:t -> field_name:string -> int32
+val get_string : scan:t -> field_name:string -> string
+val get_val : scan:t -> field_name:string -> Constant.t
+val has_field : scan:t -> field_name:string -> bool
+val close : scan:t -> unit
